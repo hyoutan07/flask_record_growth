f6090928836f5590e55d5dc5928ba6b98e948905
hyoutan07 <hyoutan0726@gmail.com>
2021-12-03 17:20:10 +0900

    make db

--- app.py
+++ app.py
@@ -2,17 +2,15 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////record.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///record.db'
 db = SQLAlchemy(app)
 
-class Post(db.Model):
+class Record(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    goal = db.Column(db.String(30), nullable=False)
+    goal = db.Column(db.String(50), nullable=False)
     detail = db.Column(db.String(200), nullable=False)
     achievement = db.Column(db.Integer, nullable=True)
     today = db.Column(db.DateTime, nullable=False)
-
-
 
 @app.route("/")
 def index():
