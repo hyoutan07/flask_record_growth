01f19e5d63bb4e8cccb6ad824e9b53a2bc5ee111
hyoutan07 <hyoutan0726@gmail.com>
2021-12-03 18:08:08 +0900

    create.html initialize today
    create OK!

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from typing import get_args
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date, datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///record.db'
@@ -10,12 +12,32 @@ class Record(db.Model):
     goal = db.Column(db.String(50), nullable=False)
     detail = db.Column(db.String(200), nullable=False)
     achievement = db.Column(db.Integer, nullable=True)
-    today = db.Column(db.DateTime, nullable=False)
+    create_at = db.Column(db.DateTime, nullable=False)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    # リクエストがGETのとき
+    if request.method == "GET":
+        posts = Record.query.all()
+        return render_template("index.html", posts = posts)
+        
+    elif request.method == "POST":
+        goal = request.form.get("goal")
+        detail = request.form.get("detail")
+        achievement = request.form.get("achievement")
+        create_at = request.form.get("create_at")
+        create_at = datetime.strptime(create_at, "%Y-%m-%d")
 
+        new_post = Record(goal=goal, detail=detail, achievement=achievement, create_at=create_at)
+        db.session.add(new_post)
+        db.session.commit()
+
+        return redirect("/")
+
+
+@app.route("/create")
+def create():
+    return render_template("create.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
