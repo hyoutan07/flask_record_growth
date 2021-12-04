ceab5d4d7b01c79237a8a40914d4608e60e39554
hyoutan07 <hyoutan0726@gmail.com>
2021-12-04 15:26:06 +0900

    add MandalaDB add create_mandala.html def index()を最適化

--- app.py
+++ app.py
@@ -8,18 +8,26 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///record.db'
 db = SQLAlchemy(app)
 
 class Record(db.Model):
+    # テーブルの名前の設定
+    __tablename__ = "Diry_Record"
     id = db.Column(db.Integer, primary_key=True) #主キー
     goal = db.Column(db.String(50), nullable=False)
     detail = db.Column(db.String(200), nullable=False)
     achievement = db.Column(db.Integer, nullable=True)
     create_at = db.Column(db.DateTime, nullable=False)
 
+class Mandala(db.Model):
+    # テーブルの名前の設定
+    __tablename__ = "Mandala_Chart"
+    id = db.Column(db.Integer, primary_key=True) #主キー
+    goal_main = db.Column(db.String(50), nullable=False)
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     # リクエストがGETのとき
     if request.method == "GET":
-        posts = Record.query.all()
-        return render_template("index.html", posts = posts)
+        goal = Mandala.query.all()
+        return render_template("index.html", posts = goal)
     
     elif request.method == "POST":
         goal = request.form.get("goal")
@@ -35,9 +43,24 @@ def index():
         return redirect("/")
 
 
-@app.route("/create")
+@app.route("/create", methods=["GET", "POST"])
 def create():
     return render_template("create.html")
+
+@app.route("/create_mandala", methods=["GET", "POST"])
+def create_mandala():
+    if request.method == "GET":
+        return render_template("create_mandala.html")
+    elif request.method == "POST":
+        goal_main = request.form.get("goal_main")
+
+        new_post = Mandala(goal_main = goal_main)
+
+        db.session.add(new_post)
+        db.session.commit()
+
+        return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
