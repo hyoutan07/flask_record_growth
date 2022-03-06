39aca822dd59cfd4c7598d90edee1fa81bb2a710
hyoutan07 <hyoutan0726@gmail.com>
2022-03-06 22:52:17 +0900

    ユーザーの追加

--- app.py
+++ app.py
@@ -26,6 +26,12 @@ class Mandala(db.Model):
     id = db.Column(db.Integer, primary_key=True) #主キー
     goal_main = db.Column(db.String(50), nullable=False)
 
+# ユーザー設定
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(50), nullable=False, unique=True)
+	password = db.Column(db.String(25))
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
